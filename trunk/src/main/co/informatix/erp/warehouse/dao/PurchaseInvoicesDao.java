309ce9a3f9901e2e1763693afdbd84ec8b9863e6
ljimenez <ljimenez@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2016-02-26 23:08:01 +0000

    Add method consultInvoicesBySupplier

--- trunk/src/main/co/informatix/erp/warehouse/dao/PurchaseInvoicesDao.java
+++ trunk/src/main/co/informatix/erp/warehouse/dao/PurchaseInvoicesDao.java
@@ -38,4 +38,29 @@ public class PurchaseInvoicesDao implements Serializable {
 		Query q = em.createQuery(query.toString());
 		return q.getResultList();
 	}
+
+	/**
+	 * This method allow consult all PurchaseInvoices stored in data base
+	 * associated with a supplier
+	 * 
+	 * @author Liseth.Jimenez
+	 * 
+	 * @param idSupplier
+	 *            : Supplier identifier for consult to purchase invoices
+	 *            associated
+	 * @return List<PurchaseInvoices>: all PurchaseInvoices stored in data base
+	 *         for a supplier
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PurchaseInvoices> consultInvoicesBySupplier(int idSupplier)
+			throws Exception {
+		StringBuilder query = new StringBuilder();
+		query.append("SELECT p FROM PurchaseInvoices p ");
+		query.append("WHERE p.suppliers.id=:idSupplier ");
+		query.append("ORDER BY p.dateTime ");
+		Query q = em.createQuery(query.toString()).setParameter("idSupplier",
+				idSupplier);
+		return q.getResultList();
+	}
 }
