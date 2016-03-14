59a4a55228c0537435fe7ddce77e72e95b1c0139
sgelves <sgelves@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2016-03-14 14:24:02 +0000

    Cambios a los DAO, ACTION y XHTML para suppliers.

--- src/main/co/informatix/erp/warehouse/dao/SuppliersDao.java
+++ src/main/co/informatix/erp/warehouse/dao/SuppliersDao.java
@@ -13,7 +13,7 @@ import co.informatix.erp.warehouse.entities.Suppliers;
 
 /**
  * DAO class that establishes the connection between business logic and
- * database. SuppliersAction used for managing Suppliers
+ * database. SuppliersAction manages Suppliers.
  * 
  * @author Mabell.Boada
  * 
@@ -25,92 +25,91 @@ public class SuppliersDao implements Serializable {
 	private EntityManager em;
 
 	/**
-	 * Edit the track information for a vendor
+	 * Edit the track information for a vendor.
 	 * 
 	 * @param suppliers
 	 *            : Supplier to edit
 	 * @throws Exception
 	 */
-	public void editarSuppliers(Suppliers suppliers) throws Exception {
+	public void editSuppliers(Suppliers suppliers) throws Exception {
 		em.merge(suppliers);
 	}
 
 	/**
-	 * Save the vendor in the database
+	 * Save the vendor in the database.
 	 * 
 	 * @param suppliers
-	 *            : Provider to save
+	 *            : Provider to save.
 	 * @throws Exception
 	 */
-	public void guardarSuppliers(Suppliers suppliers) throws Exception {
+	public void saveSuppliers(Suppliers suppliers) throws Exception {
 		em.persist(suppliers);
 	}
 
 	/**
-	 * Deleting a provider BD
+	 * Delete a provider of the database.
 	 * 
 	 * @param suppliers
-	 *            : Supplier to remove
+	 *            : Supplier to remove.
 	 * @throws Exception
 	 */
-	public void eliminarSuppliers(Suppliers suppliers) throws Exception {
+	public void deleteSuppliers(Suppliers suppliers) throws Exception {
 		em.remove(em.merge(suppliers));
 	}
 
 	/**
-	 * Returns the number of existing suppliers on the BD filtering the
-	 * information by the values sent search
+	 * Returns the number of existing suppliers on the database filtering the
+	 * information with search values.
 	 * 
-	 * @param consulta
-	 *            : String containing the query why the names of the suppliers
-	 *            are filtered
-	 * @param parametros
-	 *            : Query parameters
-	 * @return Long: Number of records found name providers
+	 * @param query
+	 *            : String containing the query with filters.
+	 * @param parameters
+	 *            : Query parameters.
+	 * @return Long: Number of records found.
 	 * @throws Exception
 	 */
-	public Long cantidadSuppliers(StringBuilder consulta,
-			List<SelectItem> parametros) throws Exception {
-		StringBuilder query = new StringBuilder();
-		query.append("SELECT COUNT(s) FROM Suppliers s ");
-		query.append(consulta);
-		Query q = em.createQuery(query.toString());
-		for (SelectItem parametro : parametros) {
-			q.setParameter(parametro.getLabel(), parametro.getValue());
+	public Long suppliersAmount(StringBuilder query, List<SelectItem> parameters)
+			throws Exception {
+		StringBuilder queryBuilder = new StringBuilder();
+		queryBuilder.append("SELECT COUNT(s) FROM Suppliers s ");
+		queryBuilder.append(query);
+		Query q = em.createQuery(queryBuilder.toString());
+		for (SelectItem parameter : parameters) {
+			q.setParameter(parameter.getLabel(), parameter.getValue());
 		}
 		return (Long) q.getSingleResult();
 	}
 
 	/**
 	 * This method queries the names of the suppliers with a certain range sent
-	 * as a parameter and filtering the information by the values sent search
+	 * as a parameter and it filters the information with search values.
 	 * 
 	 * @param start
-	 *            : Registry where consultation begins
+	 *            : Record where the query result begins.
 	 * @param range
-	 *            : Range of records
-	 * @param consult
-	 *            : Consultation records depending on the parameters selected by
-	 *            the user
+	 *            : Range of records.
+	 * @param query
+	 *            : Query records depending on the parameters selected by the
+	 *            user.
 	 * @param parameters
-	 *            : Query parameters
-	 * @return List<Suppliers>: List of names of suppliers
+	 *            : Query parameters.
+	 * @return List<Suppliers>: List of suppliers.
 	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	public List<Suppliers> consultarSuppliers(int start, int range,
-			StringBuilder consult, List<SelectItem> parameters)
-			throws Exception {
-		StringBuilder query = new StringBuilder();
-		query.append("SELECT s FROM Suppliers s ");
-		query.append(consult);
-		query.append("ORDER BY s.name ");
-		Query q = em.createQuery(query.toString());
-		for (SelectItem parametro : parameters) {
-			q.setParameter(parametro.getLabel(), parametro.getValue());
+	public List<Suppliers> suppliersFilteredSearch(int start, int range,
+			StringBuilder query, List<SelectItem> parameters) throws Exception {
+		StringBuilder queryBuilder = new StringBuilder();
+		queryBuilder.append("SELECT s FROM Suppliers s ");
+		queryBuilder.append(query);
+		queryBuilder.append("ORDER BY s.name ");
+		Query queryResult = em.createQuery(queryBuilder.toString());
+		for (SelectItem parameter : parameters) {
+			queryResult
+					.setParameter(parameter.getLabel(), parameter.getValue());
 		}
-		q.setFirstResult(start).setMaxResults(range);
-		List<Suppliers> resultList = q.getResultList();
+		queryResult.setFirstResult(start).setMaxResults(range);
+		List<Suppliers> resultList = queryResult.getResultList();
 		if (resultList.size() > 0) {
 			return resultList;
 		}
@@ -118,31 +117,30 @@ public class SuppliersDao implements Serializable {
 	}
 
 	/**
-	 * Consultation If the vendor name exists in the database when storing or
-	 * editing
+	 * Check If the vendor name exists in the database when storing or editing.
 	 * 
-	 * @param nombre
-	 *            : Supplier name to verify
+	 * @param name
+	 *            : Supplier name to verify.
 	 * @param id
-	 *            : id provider to verify
+	 *            : id provider to verify.
 	 * @return Suppliers: Object suppliers found with the search parameters name
-	 *         and id
+	 *         and id.
 	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	public Suppliers nombreExiste(String nombre, int id) throws Exception {
-		StringBuilder query = new StringBuilder();
-		query.append("SELECT s FROM Suppliers s ");
-		query.append("WHERE UPPER(s.name)=UPPER(:name) ");
+	public Suppliers nameExists(String name, int id) throws Exception {
+		StringBuilder queryBuilder = new StringBuilder();
+		queryBuilder.append("SELECT s FROM Suppliers s ");
+		queryBuilder.append("WHERE UPPER(s.name)=UPPER(:name) ");
 		if (id != 0) {
-			query.append("AND s.idSupplier <>:idSupplier ");
+			queryBuilder.append("AND s.idSupplier <>:idSupplier ");
 		}
-		Query q = em.createQuery(query.toString());
-		q.setParameter("name", nombre);
+		Query query = em.createQuery(queryBuilder.toString());
+		query.setParameter("name", name);
 		if (id != 0) {
-			q.setParameter("idSupplier", id);
+			query.setParameter("idSupplier", id);
 		}
-		List<Suppliers> results = q.getResultList();
+		List<Suppliers> results = query.getResultList();
 		if (results.size() > 0) {
 			return results.get(0);
 		}
@@ -150,28 +148,28 @@ public class SuppliersDao implements Serializable {
 	}
 
 	/**
-	 * Queries all currently DB providers.
+	 * Queries all providers who are currently in the database.
 	 * 
 	 * @author Andres.Gomez
 	 * 
-	 * @return List<Suppliers>: List of suppliers
+	 * @return List<Suppliers>: List of suppliers.
 	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	public List<Suppliers> consultarComboSuppliers() throws Exception {
+	public List<Suppliers> querySuppliers() throws Exception {
 		return em
 				.createQuery("SELECT s FROM Suppliers s " + "ORDER BY s.name ")
 				.getResultList();
 	}
 
 	/**
-	 * Suppliers method that queries the object by its ID
+	 * Suppliers method that queries an object by its ID.
 	 * 
 	 * @author Liseth.Jimenez
 	 * 
 	 * @param id
-	 *            : Suppliers identifier to consult
-	 * @return: Suppliers object found with the search parameter identifier
+	 *            : Suppliers identifier to query.
+	 * @return: Suppliers object found with the search parameter identifier.
 	 * @throws Exception
 	 */
 	public Suppliers suppliersById(int id) throws Exception {
