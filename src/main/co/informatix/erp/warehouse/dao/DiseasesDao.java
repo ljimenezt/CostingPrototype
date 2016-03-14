09e8a2b4a50042a78743ca70b091f99a90913950
jleal <jleal@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2016-03-14 19:21:43 +0000

    traduccion de dao, action y vista de Diseases, y comentario del javadoc de plot.

--- src/main/co/informatix/erp/warehouse/dao/DiseasesDao.java
+++ src/main/co/informatix/erp/warehouse/dao/DiseasesDao.java
@@ -13,7 +13,7 @@ import co.informatix.erp.warehouse.entities.Diseases;
 
 /**
  * DAO class that establishes the connection between business logic and
- * database. DiseasesAction used for managing Diseases
+ * database. DiseasesAction used for managing Diseases.
  * 
  * @author Mabell.Boada
  * 
@@ -26,80 +26,80 @@ public class DiseasesDao implements Serializable {
 	private EntityManager em;
 
 	/**
-	 * Edit the track information for a disease
+	 * Edit the track information for a disease.
 	 * 
 	 * @param diseases
-	 *            : Disease edit
+	 *            : Disease edit.
 	 * @throws Exception
 	 */
-	public void editarDiseases(Diseases diseases) throws Exception {
+	public void editDiseases(Diseases diseases) throws Exception {
 		em.merge(diseases);
 	}
 
 	/**
-	 * Save the disease in BD
+	 * Save the disease in BD.
 	 * 
 	 * @param diseases
-	 *            : Disease to save
+	 *            : Disease to save.
 	 * @throws Exception
 	 */
-	public void guardarDiseases(Diseases diseases) throws Exception {
+	public void saveDiseases(Diseases diseases) throws Exception {
 		em.persist(diseases);
 	}
 
 	/**
-	 * Eliminates a disease of the BD
+	 * Eliminates a disease of the BD.
 	 * 
 	 * @param diseases
-	 *            : Eliminate disease
+	 *            : Eliminate disease.
 	 * @throws Exception
 	 */
-	public void eliminarDiseases(Diseases diseases) throws Exception {
+	public void removeDiseases(Diseases diseases) throws Exception {
 		em.remove(em.merge(diseases));
 	}
 
 	/**
 	 * Returns the number of existing diseases in the BD filtering the
-	 * information by the values sent search
+	 * information by the values sent search.
 	 * 
-	 * @param consulta
+	 * @param consult
 	 *            : String containing the query why the names of diseases are
-	 *            filtered
-	 * @param parametros
-	 *            : Query parameters
-	 * @return Long: Number of records diseases found
+	 *            filtered.
+	 * @param parameters
+	 *            : Query parameters.
+	 * @return Long: Number of records diseases found.
 	 * @throws Exception
 	 */
-	public Long cantidadDiseases(StringBuilder consulta,
-			List<SelectItem> parametros) throws Exception {
+	public Long quantityDiseases(StringBuilder consult,
+			List<SelectItem> parameters) throws Exception {
 		StringBuilder query = new StringBuilder();
 		query.append("SELECT COUNT(d) FROM Diseases d ");
-		query.append(consulta);
+		query.append(consult);
 		Query q = em.createQuery(query.toString());
-		for (SelectItem parametro : parametros) {
-			q.setParameter(parametro.getLabel(), parametro.getValue());
+		for (SelectItem parameter : parameters) {
+			q.setParameter(parameter.getLabel(), parameter.getValue());
 		}
 		return (Long) q.getSingleResult();
 	}
 
 	/**
 	 * This method see the names of diseases with a certain range sent as a
-	 * parameter and filtering the information by the values sent search
+	 * parameter and filtering the information by the values sent search.
 	 * 
 	 * @param start
-	 *            : Registry where consultation begins
+	 *            : Registry where consultation begins.
 	 * @param range
-	 *            : Range of records
+	 *            : Range of records.
 	 * @param consult
 	 *            : Consultation records depending on the parameters selected by
-	 *            the user
+	 *            the user.
 	 * @param parameters
-	 *            : Query parameters
-	 * @return List<Diseases>: Listing the names of diseases
+	 *            : Query parameters.
+	 * @return List<Diseases>: Listing the names of diseases.
 	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	public List<Diseases> consultarDiseases(int start, int range,
+	public List<Diseases> consultDiseases(int start, int range,
 			StringBuilder consult, List<SelectItem> parameters)
 			throws Exception {
 		StringBuilder query = new StringBuilder();
@@ -107,8 +107,8 @@ public class DiseasesDao implements Serializable {
 		query.append(consult);
 		query.append("ORDER BY d.name ");
 		Query q = em.createQuery(query.toString());
-		for (SelectItem parametro : parameters) {
-			q.setParameter(parametro.getLabel(), parametro.getValue());
+		for (SelectItem parameter : parameters) {
+			q.setParameter(parameter.getLabel(), parameter.getValue());
 		}
 		q.setFirstResult(start).setMaxResults(range);
 		List<Diseases> resultList = q.getResultList();
@@ -120,17 +120,17 @@ public class DiseasesDao implements Serializable {
 
 	/**
 	 * Consultation if the name of the disease exists in the database when
-	 * storing or editing
+	 * storing or editing.
 	 * 
-	 * @param nombre
-	 *            : Name of the disease to verify
+	 * @param name
+	 *            : Name of the disease to verify.
 	 * @param id
-	 *            : id disease to verify
-	 * @return Diseases: Object found with the search parameters name and id
+	 *            : id disease to verify.
+	 * @return Diseases: Object found with the search parameters name and id.
 	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	public Diseases nombreExiste(String nombre, int id) throws Exception {
+	public Diseases nameExist(String name, int id) throws Exception {
 		StringBuilder query = new StringBuilder();
 		query.append("SELECT d FROM Diseases d ");
 		query.append("WHERE UPPER(d.name)=UPPER(:name) ");
@@ -138,7 +138,7 @@ public class DiseasesDao implements Serializable {
 			query.append("AND d.idDisease <>:idDisease ");
 		}
 		Query q = em.createQuery(query.toString());
-		q.setParameter("name", nombre);
+		q.setParameter("name", name);
 		if (id != 0) {
 			q.setParameter("idDisease", id);
 		}
