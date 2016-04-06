b965990bfc528010587ed6ab0cba3df2f6febad7
jleal <jleal@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2016-04-06 22:14:05 +0000

    correccion, solo guarda y edita

--- trunk/src/main/co/informatix/erp/seguridad/dao/GestionarPerfilSistemaDao.java
+++ trunk/src/main/co/informatix/erp/seguridad/dao/GestionarPerfilSistemaDao.java
@@ -1,13 +1,10 @@
 package co.informatix.erp.seguridad.dao;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.faces.model.SelectItem;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 import co.informatix.erp.utils.ConstantesErp;
 import co.informatix.security.entities.PerfilSistema;
@@ -25,41 +22,6 @@ public class GestionarPerfilSistemaDao implements Serializable {
 
 	@PersistenceContext(unitName = "ERPImp")
 	private EntityManager em;
-
-	/**
-	 * This method allows consulting profile system with a certain range sent as
-	 * a parameter and filtering the information sent search values.
-	 * 
-	 * @param start
-	 *            :where he started the consultation record.
-	 * @param range
-	 *            : range of records.
-	 * @param consult
-	 *            : Query records depending on the user selected parameter.
-	 * @param parameters
-	 *            : consult parameters.
-	 * @return List<PerfilSistema>: list of the profile system.
-	 * @throws Exception
-	 */
-	@SuppressWarnings("unchecked")
-	public List<PerfilSistema> consultProfileSystem(int start, int range,
-			StringBuilder consult, List<SelectItem> parameters)
-			throws Exception {
-		StringBuilder query = new StringBuilder();
-		query.append("SELECT ps FROM PerfilSistema ps ");
-		query.append(consult);
-		query.append("ORDER BY ps.emailServerUser ");
-		Query q = em.createQuery(query.toString());
-		for (SelectItem parameter : parameters) {
-			q.setParameter(parameter.getLabel(), parameter.getValue());
-		}
-		q.setFirstResult(start).setMaxResults(range);
-		List<PerfilSistema> resultList = q.getResultList();
-		if (resultList.size() > 0) {
-			return resultList;
-		}
-		return null;
-	}
 
 	/**
 	 * Save a profile system for users in database.
@@ -93,44 +55,6 @@ public class GestionarPerfilSistemaDao implements Serializable {
 	public void removeProfileSystem(PerfilSistema perfilSistema)
 			throws Exception {
 		em.remove(em.merge(perfilSistema));
-	}
-
-	/**
-	 * Returns the number of existing profile systems in the database by
-	 * filtering information sent lookup values.
-	 * 
-	 * @param consult
-	 *            : String containing the query for which the properties are
-	 *            filtered.
-	 * @param parameters
-	 *            : query parameters.
-	 * @return Long: amount of profile systems records found.
-	 * @throws Exception
-	 */
-	public Long quantityProfileSystem(StringBuilder consult,
-			List<SelectItem> parameters) throws Exception {
-		StringBuilder query = new StringBuilder();
-		query.append("SELECT COUNT(ps) FROM PerfilSistema ps ");
-		query.append(consult);
-		Query q = em.createQuery(query.toString());
-		for (SelectItem parameter : parameters) {
-			q.setParameter(parameter.getLabel(), parameter.getValue());
-		}
-		return (Long) q.getSingleResult();
-	}
-
-	/**
-	 * Profile system method that queries the object and saves it to a list.
-	 * 
-	 * @return List<PerfilSistema>: List of profile system.
-	 * @throws Exception
-	 */
-	@SuppressWarnings("unchecked")
-	public List<PerfilSistema> listProfileSystem() throws Exception {
-		Query q = em.createQuery("SELECT ps FROM PerfilSistema ps "
-				+ " ORDER BY ps.emailServerUser ASC");
-		return q.getResultList();
-
 	}
 
 	/**
