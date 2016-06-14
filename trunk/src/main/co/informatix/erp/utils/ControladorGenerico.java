4dafa1a717633825a21a9bf66cd4e58cfd907fa0
gherrera <gherrera@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2016-06-14 22:58:46 +0000

    Bug 1932

--- trunk/src/main/co/informatix/erp/utils/ControladorGenerico.java
+++ trunk/src/main/co/informatix/erp/utils/ControladorGenerico.java
@@ -1,17 +1,8 @@
 package co.informatix.erp.utils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.Serializable;
 
-import javax.faces.context.FacesContext;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.io.FilenameUtils;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * Esta clase permite realizar la logica del negocio para los metodos genericos
@@ -21,83 +12,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 @SuppressWarnings("serial")
 public class ControladorGenerico implements Serializable {
-
-	/**
-	 * Este metodo permite al usuario descargar un archivo ubicado en la ruta
-	 * que recibe como parametro
-	 * 
-	 * @param URL
-	 *            Ruta del archivo a descargar
-	 * 
-	 * @param tipoArchivo
-	 *            Indica la extension del archivo (pdf, xlsx)
-	 * 
-	 * @throws IOException
-	 */
-	public static void descargarFormato(String URL, String tipoArchivo)
-			throws IOException {
-		File reporte = new File(URL);
-		FacesContext ctx = FacesContext.getCurrentInstance();
-		FileInputStream fis = new FileInputStream(reporte);
-		byte[] bytes = new byte[1000];
-		int read = 0;
-		if (!ctx.getResponseComplete()) {
-			String fileName = reporte.getName();
-			String contentType;
-			if (tipoArchivo.equals("pdf")) {
-				contentType = "application/pdf";
-			} else {
-				contentType = "application/vnd.ms-excel";
-			}
-			HttpServletResponse response = (HttpServletResponse) ctx
-					.getExternalContext().getResponse();
-			response.setContentType(contentType);
-			response.setHeader("Content-Disposition", "attachment;filename=\""
-					+ fileName + "\"");
-			ServletOutputStream out = response.getOutputStream();
-			while ((read = fis.read(bytes)) != -1) {
-				out.write(bytes, 0, read);
-			}
-			out.flush();
-			out.close();
-			fis.close();
-			ctx.responseComplete();
-		}
-	}
-
-	/**
-	 * Metodo que toma una imagen y la retorna lista para cargar en el archivo
-	 * de excel
-	 * 
-	 * @param path
-	 *            : Ruta de la imagen a cargar en el reporte
-	 * @param wb
-	 *            : libro donde se agregara la imagen referenciada
-	 * @return int: Retorna un valor diferente de cero cuando el procedimiento
-	 *         se ejecute correctamente
-	 * @throws IOException
-	 */
-	public static int cargarImagen(File path, XSSFWorkbook wb)
-			throws IOException {
-		int pictureIndex;
-		FileInputStream fis = null;
-		ByteArrayOutputStream bos = null;
-		try {
-			fis = new FileInputStream(path);
-			bos = new ByteArrayOutputStream();
-			int c;
-			while ((c = fis.read()) != -1)
-				bos.write(c);
-			pictureIndex = wb.addPicture(bos.toByteArray(),
-					XSSFWorkbook.PICTURE_TYPE_PNG);
-		} finally {
-			if (fis != null)
-				fis.close();
-			if (bos != null)
-				bos.close();
-		}
-		return pictureIndex;
-	}
 
 	/**
 	 * This method allows you to consult the route prefix 'C: /' windows server
