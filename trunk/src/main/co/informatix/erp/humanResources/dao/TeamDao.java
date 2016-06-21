51a0567de47e4f383b481730582ddeb356f48f8c
sboada <sboada@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2016-06-21 20:39:53 +0000

    team icon and TeamDao

---
+++ trunk/src/main/co/informatix/erp/humanResources/dao/TeamDao.java
@@ -0,0 +1,118 @@
+package co.informatix.erp.humanResources.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.faces.model.SelectItem;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import co.informatix.erp.humanResources.entities.Team;
+
+/**
+ * class DAO that establishes the connection between business logic and database
+ * for handling the Team entity.
+ * 
+ * @author Wilhelm.Boada
+ * 
+ */
+@SuppressWarnings("serial")
+@Stateless
+public class TeamDao implements Serializable {
+	@PersistenceContext(unitName = "ERPImp")
+	private EntityManager em;
+
+	/**
+	 * Save an team in database
+	 * 
+	 * @param team
+	 *            : team to save.
+	 * @throws Exception
+	 */
+	public void saveTeam(Team team) throws Exception {
+		em.persist(team);
+	}
+
+	/**
+	 * Edit an team in database
+	 * 
+	 * @param team
+	 *            : team to edit.
+	 * @throws Exception
+	 */
+	public void editTeam(Team team) throws Exception {
+		em.merge(team);
+	}
+
+	/**
+	 * Delete the teams of the database.
+	 * 
+	 * @param team
+	 *            : team to remove.
+	 * @throws Exception
+	 */
+	public void deleteTeam(Team team) throws Exception {
+		em.remove(em.merge(team));
+	}
+
+	/**
+	 * Returns the number of existing team in the database filtering information
+	 * search by the values sent.
+	 * 
+	 * @param consult
+	 *            : String containing the query why the filter purchase
+	 *            invoices.
+	 * @param parameters
+	 *            : query parameters.
+	 * @return Long: number of teams records found.
+	 * @throws Exception
+	 */
+	public Long amountTeam(StringBuilder consult, List<SelectItem> parameters)
+			throws Exception {
+		StringBuilder query = new StringBuilder();
+		query.append("SELECT COUNT(t) FROM Team t ");
+		query.append(consult);
+		Query q = em.createQuery(query.toString());
+		for (SelectItem parameter : parameters) {
+			q.setParameter(parameter.getLabel(), parameter.getValue());
+		}
+		return (Long) q.getSingleResult();
+	}
+
+	/**
+	 * This method consult team list with a certain range sent as a parameter
+	 * and filtering the information by the values of sent search.
+	 * 
+	 * @param start
+	 *            : The record where the query result starts.
+	 * @param range
+	 *            : Range of records.
+	 * @param consult
+	 *            : Query records depending on the user selected parameter.
+	 * @param parameters
+	 *            : Query parameters.
+	 * @return List<Team>: teams list
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Team> consultTeams(int start, int range, StringBuilder consult,
+			List<SelectItem> parameters) throws Exception {
+		StringBuilder query = new StringBuilder();
+		query.append("SELECT t FROM Team t ");
+		query.append(consult);
+		query.append("ORDER BY t.name ");
+		Query q = em.createQuery(query.toString());
+		for (SelectItem parameter : parameters) {
+			q.setParameter(parameter.getLabel(), parameter.getValue());
+		}
+		q.setFirstResult(start).setMaxResults(range);
+		List<Team> resultList = q.getResultList();
+		if (resultList.size() > 0) {
+			return resultList;
+		}
+		return null;
+	}
+
+}
