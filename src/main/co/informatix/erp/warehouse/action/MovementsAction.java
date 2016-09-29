f515ca42936b20e7f103748dd008af71f68fabf3
sboada <sboada@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2016-09-29 20:06:02 +0000

    Create manage Withdrawal

--- src/main/co/informatix/erp/warehouse/action/MovementsAction.java
+++ src/main/co/informatix/erp/warehouse/action/MovementsAction.java
@@ -15,7 +15,11 @@ import javax.faces.model.SelectItem;
 import javax.inject.Inject;
 import javax.transaction.UserTransaction;
 
+import co.informatix.erp.costs.dao.ActivitiesDao;
+import co.informatix.erp.costs.dao.ActivityMaterialsDao;
 import co.informatix.erp.costs.entities.Activities;
+import co.informatix.erp.costs.entities.ActivityMaterials;
+import co.informatix.erp.humanResources.entities.Hr;
 import co.informatix.erp.utils.Constantes;
 import co.informatix.erp.utils.ControladorContexto;
 import co.informatix.erp.utils.ControladorFechas;
@@ -42,108 +46,35 @@ import co.informatix.security.action.IdentityAction;
 @RequestScoped
 public class MovementsAction implements Serializable {
 
-	private List<Activities> activitiesList;
-	private Paginador pagination = new Paginador();
-
-	/**
-	 * @return activitiesList: list of activity objects.
-	 */
-	public List<Activities> getActivitiesList() {
-		return activitiesList;
-	}
-
-	/**
-	 * @param activitiesList
-	 *            : list of activity objects.
-	 */
-	public void setActivitiesList(List<Activities> activitiesList) {
-		this.activitiesList = activitiesList;
-	}
-
-	/**
-	 * @return pagination: Management paged list of activities.
-	 */
-	public Paginador getPagination() {
-		return pagination;
-	}
-
-	/**
-	 * @param pagination
-	 *            : Management paged list of activities.
-	 */
-	public void setPagination(Paginador pagination) {
-		this.pagination = pagination;
-	}
-
-	// Field To Logic Expiration
-
-	private Paginador paginationExpire = new Paginador();
-
 	@EJB
 	private DepositsDao depositsDao;
 	@EJB
 	private TransactionTypeDao transactionTypeDao;
-	@Resource
-	private UserTransaction userTransaction;
-	@Inject
-	private IdentityAction identity;
 	@EJB
 	private TransactionsDao transactionsDao;
-	private boolean selected = false;
-
-	private Integer rangeExpiration;
-	private String nameSearch;
+	@EJB
+	private ActivitiesDao activitiesDao;
+	@EJB
+	private ActivityMaterialsDao activityMaterialsDao;
+	@Inject
+	private IdentityAction identity;
+	@Resource
+	private UserTransaction userTransaction;
 
 	private List<Deposits> listDepositsExpired;
 	private List<Deposits> listDepositsExpiredSelected;
 	private List<Integer> rangeItems;
-
-	/**
-	 * @return paginationExpire :Management paged list of deposits expire.
-	 */
-	public Paginador getPaginationExpire() {
-		return paginationExpire;
-	}
-
-	/**
-	 * @param paginationExpire
-	 *            :Management paged list of deposits expire.
-	 */
-	public void setPaginationExpire(Paginador paginationExpire) {
-		this.paginationExpire = paginationExpire;
-	}
-
-	/**
-	 * @return rangeExpiration: number of the day that user selected to consult
-	 *         the materials expired
-	 */
-	public Integer getRangeExpiration() {
-		return rangeExpiration;
-	}
-
-	/**
-	 * @return nameSearch : Material name to search.
-	 */
-	public String getNameSearch() {
-		return nameSearch;
-	}
-
-	/**
-	 * @param nameSearch
-	 *            : Material name to search.
-	 */
-	public void setNameSearch(String nameSearch) {
-		this.nameSearch = nameSearch;
-	}
-
-	/**
-	 * @param rangeExpiration
-	 *            :number of the day that user selected to consult the materials
-	 *            expired
-	 */
-	public void setRangeExpiration(Integer rangeExpiration) {
-		this.rangeExpiration = rangeExpiration;
-	}
+	private List<Activities> activitiesList;
+	private List<ActivityMaterials> listActivityMaterials;
+	private List<ActivityMaterials> listActivityMaterialsSelected;
+	private Paginador pagination = new Paginador();
+	private Paginador paginationExpire = new Paginador();
+	private Activities activityActualSelected;
+	private ActivityMaterials activityMaterials;
+	private Hr hr;
+	private Integer rangeExpiration;
+	private String nameSearch;
+	private boolean selected = false;
 
 	/**
 	 * @return listDepositsExpired : List of the deposits with materials expired
@@ -178,6 +109,178 @@ public class MovementsAction implements Serializable {
 	}
 
 	/**
+	 * @return rangeItems :List of items from expiration of movements are loaded
+	 *         into the combo in the user interface.
+	 */
+	public List<Integer> getRangeItems() {
+		return rangeItems;
+	}
+
+	/**
+	 * @param rangeItems
+	 *            :List of items from expiration of movements are loaded into
+	 *            the combo in the user interface.
+	 */
+	public void setRangeItems(List<Integer> rangeItems) {
+		this.rangeItems = rangeItems;
+	}
+
+	/**
+	 * @return activitiesList: list of activity objects.
+	 */
+	public List<Activities> getActivitiesList() {
+		return activitiesList;
+	}
+
+	/**
+	 * @param activitiesList
+	 *            : list of activity objects.
+	 */
+	public void setActivitiesList(List<Activities> activitiesList) {
+		this.activitiesList = activitiesList;
+	}
+
+	/**
+	 * @return listActivityMaterials: material list assigned to the activity.
+	 */
+	public List<ActivityMaterials> getListActivityMaterials() {
+		return listActivityMaterials;
+	}
+
+	/**
+	 * @param listActivityMaterials
+	 *            : material list assigned to the activity.
+	 */
+	public void setListActivityMaterials(
+			List<ActivityMaterials> listActivityMaterials) {
+		this.listActivityMaterials = listActivityMaterials;
+	}
+
+	/**
+	 * @return listActivityMaterialsSelected: material list selected to the
+	 *         activity.
+	 */
+	public List<ActivityMaterials> getListActivityMaterialsSelected() {
+		return listActivityMaterialsSelected;
+	}
+
+	/**
+	 * @param listActivityMaterialsSelected
+	 *            : material list selected to the activity.
+	 */
+	public void setListActivityMaterialsSelected(
+			List<ActivityMaterials> listActivityMaterialsSelected) {
+		this.listActivityMaterialsSelected = listActivityMaterialsSelected;
+	}
+
+	/**
+	 * @return pagination: Management paged list of activities.
+	 */
+	public Paginador getPagination() {
+		return pagination;
+	}
+
+	/**
+	 * @param pagination
+	 *            : Management paged list of activities.
+	 */
+	public void setPagination(Paginador pagination) {
+		this.pagination = pagination;
+	}
+
+	/**
+	 * @return paginationExpire :Management paged list of deposits expire.
+	 */
+	public Paginador getPaginationExpire() {
+		return paginationExpire;
+	}
+
+	/**
+	 * @param paginationExpire
+	 *            :Management paged list of deposits expire.
+	 */
+	public void setPaginationExpire(Paginador paginationExpire) {
+		this.paginationExpire = paginationExpire;
+	}
+
+	/**
+	 * @return activityActualSelected: activity selected in the activity table.
+	 */
+	public Activities getActivityActualSelected() {
+		return activityActualSelected;
+	}
+
+	/**
+	 * @param activityActualSelected
+	 *            : activity selected in the activity table.
+	 */
+	public void setActivityActualSelected(Activities activityActualSelected) {
+		this.activityActualSelected = activityActualSelected;
+	}
+
+	/**
+	 * @return activityMaterials: activityMaterial for the transaction.
+	 */
+	public ActivityMaterials getActivityMaterials() {
+		return activityMaterials;
+	}
+
+	/**
+	 * @param activityMaterials
+	 *            : activityMaterial for the transaction.
+	 */
+	public void setActivityMaterials(ActivityMaterials activityMaterials) {
+		this.activityMaterials = activityMaterials;
+	}
+
+	/**
+	 * @return hr: hr belonging to the transaction.
+	 */
+	public Hr getHr() {
+		return hr;
+	}
+
+	/**
+	 * @param hr
+	 *            : hr belonging to the transaction.
+	 */
+	public void setHr(Hr hr) {
+		this.hr = hr;
+	}
+
+	/**
+	 * @return rangeExpiration: number of the day that user selected to consult
+	 *         the materials expired
+	 */
+	public Integer getRangeExpiration() {
+		return rangeExpiration;
+	}
+
+	/**
+	 * @param rangeExpiration
+	 *            :number of the day that user selected to consult the materials
+	 *            expired
+	 */
+	public void setRangeExpiration(Integer rangeExpiration) {
+		this.rangeExpiration = rangeExpiration;
+	}
+
+	/**
+	 * @return nameSearch : Material name to search.
+	 */
+	public String getNameSearch() {
+		return nameSearch;
+	}
+
+	/**
+	 * @param nameSearch
+	 *            : Material name to search.
+	 */
+	public void setNameSearch(String nameSearch) {
+		this.nameSearch = nameSearch;
+	}
+
+	/**
 	 * @return selected : boolean flag to mark all deposits selected
 	 */
 	public boolean isSelected() {
@@ -193,20 +296,251 @@ public class MovementsAction implements Serializable {
 	}
 
 	/**
-	 * @return rangeItems :List of items from expiration of movements are loaded
-	 *         into the combo in the user interface.
+	 * Method to initialize the fields in the search.
+	 * 
+	 * @return consultActivities: Method that consultation activities and load
+	 *         the template with the information found.
 	 */
-	public List<Integer> getRangeItems() {
-		return rangeItems;
+	public String initializeWithdraws() {
+		this.hr = new Hr();
+		return consultActivities();
 	}
 
 	/**
-	 * @param rangeItems
-	 *            :List of items from expiration of movements are loaded into
-	 *            the combo in the user interface.
+	 * See the existing activities list.
+	 * 
+	 * @return gesWithdraws: Navigation rule that redirects to manage the
+	 *         withdraws.
 	 */
-	public void setRangeItems(List<Integer> rangeItems) {
-		this.rangeItems = rangeItems;
+	public String consultActivities() {
+		ResourceBundle bundle = ControladorContexto.getBundle("mensaje");
+		ResourceBundle bundleCostos = ControladorContexto
+				.getBundle("messageCosts");
+		ValidacionesAction validations = (ValidacionesAction) ControladorContexto
+				.getContextBean(ValidacionesAction.class);
+		List<SelectItem> parameters = new ArrayList<SelectItem>();
+		StringBuilder query = new StringBuilder();
+		StringBuilder unionSearchMessages = new StringBuilder();
+		String searchMessages = "";
+		try {
+			Long quantity = activitiesDao.amountActivitiesByActivityMaterials(
+					query, parameters);
+			if (quantity != null && quantity > 0) {
+				pagination.paginar(quantity);
+
+				this.activitiesList = activitiesDao
+						.queryActivitiesByActivityMaterials(
+								pagination.getInicio(), pagination.getRango(),
+								query, parameters);
+			}
+			if ((this.activitiesList == null || activitiesList.size() <= 0)
+					&& !"".equals(unionSearchMessages.toString())) {
+				searchMessages = MessageFormat
+						.format(bundle
+								.getString("message_no_existen_registros_criterio_busqueda"),
+								unionSearchMessages);
+			} else if (activitiesList == null || activitiesList.size() <= 0) {
+				ControladorContexto.mensajeInformacion(null,
+						bundle.getString("message_not_exists_activities_hr"));
+			} else if (!"".equals(unionSearchMessages.toString())) {
+				searchMessages = MessageFormat
+						.format(bundle
+								.getString("message_existen_registros_criterio_busqueda"),
+								bundleCostos.getString("activities_label_s"),
+								unionSearchMessages);
+			}
+			validations.setMensajeBusqueda(searchMessages);
+		} catch (Exception e) {
+			ControladorContexto.mensajeError(e);
+		}
+		return "gesWithdraws";
+	}
+
+	/**
+	 * Selects a single activity for display the associated transaction.
+	 * 
+	 * @param activitySelected
+	 *            : activity selected on the view.
+	 */
+	public void selectActivity(Activities activitySelected) {
+		this.activityActualSelected = new Activities();
+		this.listActivityMaterialsSelected = new ArrayList<ActivityMaterials>();
+		activitySelected.setSeleccionado(true);
+		for (Activities activity : activitiesList) {
+			if (activity.isSeleccionado()) {
+				if (activity.getIdActivity() == activitySelected
+						.getIdActivity()) {
+					this.activityActualSelected = activity;
+				} else {
+					activity.setSeleccionado(false);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Method to clean the HR associated with the transaction.
+	 */
+	public void cleanHr() {
+		this.hr = new Hr();
+	}
+
+	/**
+	 * Method to load the selected HR.
+	 * 
+	 * @param hr
+	 *            : object HR selected.
+	 */
+	public void loadHr(Hr hr) {
+		this.hr = hr;
+	}
+
+	/**
+	 * Consult the material list associated to the activity.
+	 * 
+	 */
+	public void consultMaterialsByActivity() {
+		ValidacionesAction validations = ControladorContexto
+				.getContextBean(ValidacionesAction.class);
+		List<SelectItem> parameters = new ArrayList<SelectItem>();
+		StringBuilder consult = new StringBuilder();
+		String messageSearch = "";
+		try {
+			advancedSearchMaterialsByActivity(consult, parameters);
+			Long quantity = activityMaterialsDao.amountMaterialsByActivity(
+					consult, parameters);
+			if (quantity != null) {
+				paginationExpire.paginarRangoDefinido(quantity, 5);
+				this.listActivityMaterials = activityMaterialsDao
+						.queryMaterialsByActivity(paginationExpire.getInicio(),
+								paginationExpire.getRango(), consult,
+								parameters);
+			}
+			if (this.listActivityMaterialsSelected != null
+					&& this.listActivityMaterialsSelected.size() >= 0) {
+				persistentActivityMaterialsSelected();
+			}
+			validations.setMensajeBusqueda(messageSearch);
+		} catch (Exception e) {
+			ControladorContexto.mensajeError(e);
+		}
+	}
+
+	/**
+	 * This method builds the query for an advanced search for relations between
+	 * activities and materials, it also builds display messages depending on
+	 * the search criteria selected by the user.
+	 * 
+	 * @param consult
+	 *            : query to concatenate.
+	 * @param parameters
+	 *            : list of the search parameters.
+	 */
+	private void advancedSearchMaterialsByActivity(StringBuilder consult,
+			List<SelectItem> parameters) {
+		consult.append("WHERE ac.idActivity = :id ");
+		SelectItem item = new SelectItem(
+				this.activityActualSelected.getIdActivity(), "id");
+		parameters.add(item);
+	}
+
+	/**
+	 * Selects a single activityMaterials for display the associated
+	 * transaction.
+	 * 
+	 * @param flag
+	 *            : this field validate if activityMaterials is selected.
+	 */
+	public void selectActivityMaterials(boolean flag) {
+		activityMaterials.setSelected(flag);
+		if (activityMaterials.isSelected()) {
+			this.listActivityMaterialsSelected.add(activityMaterials);
+		} else {
+			this.listActivityMaterialsSelected.remove(activityMaterials);
+		}
+
+	}
+
+	/**
+	 * This method allows maintaining list of selected activityMaterials whether
+	 * the search of activityMaterials is run again.
+	 * 
+	 */
+	public void persistentActivityMaterialsSelected() {
+		for (ActivityMaterials activityMaterial : listActivityMaterials) {
+			for (ActivityMaterials activityMaterialSelected : listActivityMaterialsSelected) {
+				if (activityMaterial.getActivityMaterialsPK().getMaterials()
+						.getIdMaterial() == activityMaterialSelected
+						.getActivityMaterialsPK().getMaterials()
+						.getIdMaterial()) {
+					activityMaterial.setSelected(true);
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method allows create the withdrawal transaction and edit the
+	 * deposits.
+	 * 
+	 */
+	public void createTransactionWithdrawal() {
+		List<Deposits> depositsListEdit = new ArrayList<Deposits>();
+		List<Transactions> transactionsList = new ArrayList<Transactions>();
+		try {
+			if (this.listActivityMaterialsSelected != null
+					&& this.listActivityMaterialsSelected.size() >= 0) {
+				for (ActivityMaterials activityMaterials : this.listActivityMaterialsSelected) {
+					if (activityMaterials.getQuantityBudget() > 0) {
+						double amount = activityMaterials.getQuantityActual();
+						List<Deposits> depositsListActual = depositsDao
+								.consultDepositsActualQuantityById(activityMaterials
+										.getActivityMaterialsPK()
+										.getMaterials().getIdMaterial());
+						TransactionType transactionType = transactionTypeDao
+								.transactionTypeById(Constantes.TRANSACTION_TYPE_ID_WITHDRAWAL);
+						while (amount > 0) {
+							Transactions transactions = new Transactions();
+							Deposits depositsActual = depositsListActual.get(0);
+							if (amount > depositsActual.getActualQuantity()) {
+								transactions.setQuantity(depositsActual
+										.getActualQuantity());
+								amount = amount
+										- depositsActual.getActualQuantity();
+								depositsListActual.remove(depositsActual);
+								depositsActual.setActualQuantity(0.0);
+							} else {
+								transactions.setQuantity(amount);
+								depositsListActual.remove(depositsActual);
+								depositsActual.setActualQuantity(depositsActual
+										.getActualQuantity() - amount);
+								amount = 0;
+							}
+							depositsListEdit.add(depositsActual);
+							transactions.setTransactionType(transactionType);
+							transactions.setDateTime(new Date());
+							transactions.setDeposits(depositsActual);
+							transactions.setHr(hr);
+							transactions
+									.setActivities(this.activityActualSelected);
+							transactionsList.add(transactions);
+						}
+					}
+				}
+				if (transactionsList != null) {
+					for (Transactions transactions : transactionsList) {
+						transactionsDao.saveTransaction(transactions);
+					}
+				}
+				if (depositsListEdit != null) {
+					for (Deposits deposits : depositsListEdit) {
+						depositsDao.editDeposits(deposits);
+					}
+				}
+			}
+		} catch (Exception e) {
+			ControladorContexto.mensajeError(e);
+		}
 	}
 
 	/**
