8e4d1f122381a210143d538e2933e6eeff83d85c
lgranados <lgranados@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2016-10-26 21:38:17 +0000

    create register for novelty type

--- src/main/co/informatix/erp/humanResources/action/NoveltyTypeAction.java
+++ src/main/co/informatix/erp/humanResources/action/NoveltyTypeAction.java
@@ -1,11 +1,20 @@
 package co.informatix.erp.humanResources.action;
 
 import java.io.Serializable;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
 
+import co.informatix.erp.humanResources.dao.NoveltyTypeDao;
 import co.informatix.erp.humanResources.entities.NoveltyType;
+import co.informatix.erp.utils.ControladorContexto;
+import co.informatix.erp.utils.EncodeFilter;
 
 /**
  * This class implements the logic related to create, update, and delete types
@@ -20,8 +29,11 @@ public class NoveltyTypeAction implements Serializable {
 
 	private NoveltyType noveltyType;
 
+	@EJB
+	private NoveltyTypeDao noveltyTypeDao;
+
 	/**
-	 * @return
+	 * @return noveltyType: Object of the novelty type.
 	 */
 	public NoveltyType getNoveltyType() {
 		return noveltyType;
@@ -29,17 +41,19 @@ public class NoveltyTypeAction implements Serializable {
 
 	/**
 	 * @param noveltyType
+	 *            : Object of the novelty type.
 	 */
 	public void setNoveltyType(NoveltyType noveltyType) {
 		this.noveltyType = noveltyType;
 	}
 
 	/**
-	 * Method to edit or create a new type of novelty.
+	 * Method to edit or create a new novelty type.
 	 * 
 	 * @param noveltyType
 	 *            : Type of novelty that will add or edit.
-	 * @return regNoveltyType: Redirected to the template registerNoveltyType.
+	 * @return regNoveltyType: Navigation rule that redirects to the template
+	 *         registerNoveltyType.
 	 */
 	public String addEditNoveltyType(NoveltyType noveltyType) {
 		if (noveltyType != null) {
@@ -50,4 +64,70 @@ public class NoveltyTypeAction implements Serializable {
 		return "regNoveltyType";
 	}
 
+	/**
+	 * Method used to save or edit the novelty type.
+	 * 
+	 * @return consultNoveltyType(): method that redirects to manage the types
+	 *         of novelty.
+	 */
+	public String saveNoveltyType() {
+		ResourceBundle bundle = ControladorContexto.getBundle("mensaje");
+		String messageLog = "message_registro_modificar";
+		try {
+			if (noveltyType.getId() != 0) {
+				noveltyTypeDao.editNoveltyType(noveltyType);
+			} else {
+				messageLog = "message_registro_guardar";
+				noveltyTypeDao.saveNoveltyType(noveltyType);
+			}
+			ControladorContexto.mensajeInformacion(null, MessageFormat.format(
+					bundle.getString(messageLog), noveltyType.getName()));
+		} catch (Exception e) {
+			ControladorContexto.mensajeError(e);
+		}
+		return consultNoveltyType();
+	}
+
+	/**
+	 * Consult the list of the novelty type.
+	 * 
+	 * @return gesContrType: Navigation rule that redirects to manage contract
+	 *         types.
+	 */
+	public String consultNoveltyType() {
+		return "";
+	}
+
+	/**
+	 * To validate the name of the novelty types, to not repeat in the database
+	 * and validates against XSS.
+	 * 
+	 * @param context
+	 *            : Application context.
+	 * @param toValidate
+	 *            : Validate component.
+	 * @param value
+	 *            : Field value is validated.
+	 */
+	public void validateNameXSS(FacesContext context, UIComponent toValidate,
+			Object value) {
+		String name = (String) value;
+		String clientId = toValidate.getClientId(context);
+		try {
+			NoveltyType noveltyTypeAux = noveltyTypeDao.nameExists(name,
+					noveltyType.getId());
+			if (noveltyTypeAux != null) {
+				String messageExistence = "message_ya_existe_verifique";
+				ControladorContexto.mensajeErrorEspecifico(clientId,
+						messageExistence, "mensaje");
+				((UIInput) toValidate).setValid(false);
+			}
+			if (!EncodeFilter.validarXSS(name, clientId,
+					"locate.regex.letras.numeros")) {
+				((UIInput) toValidate).setValid(false);
+			}
+		} catch (Exception e) {
+			ControladorContexto.mensajeError(e);
+		}
+	}
 }
--- src/main/co/informatix/erp/humanResources/dao/NoveltyTypeDao.java
+++ src/main/co/informatix/erp/humanResources/dao/NoveltyTypeDao.java
@@ -1,7 +1,12 @@
 package co.informatix.erp.humanResources.dao;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import co.informatix.erp.humanResources.entities.NoveltyType;
 
@@ -11,7 +16,9 @@ import co.informatix.erp.humanResources.entities.NoveltyType;
  * 
  * @author Claudia.Rey
  */
-public class NoveltyTypeDao {
+@SuppressWarnings("serial")
+@Stateless
+public class NoveltyTypeDao implements Serializable {
 
 	@PersistenceContext(unitName = "ERPImp")
 	private EntityManager em;
@@ -37,7 +44,7 @@ public class NoveltyTypeDao {
 	public void editNoveltyType(NoveltyType noveltyType) throws Exception {
 		em.merge(noveltyType);
 	}
-	
+
 	/**
 	 * Removes a type of novelty from the database.
 	 * 
@@ -48,5 +55,37 @@ public class NoveltyTypeDao {
 	public void removeNoveltyType(NoveltyType noveltyType) throws Exception {
 		em.remove(em.merge(noveltyType));
 	}
-	
+
+	/**
+	 * Consult if the name of the novelty type exist in the database when saving
+	 * or editing.
+	 * 
+	 * @author Luna.Granados
+	 * 
+	 * @param name
+	 *            : Name the type of novelty to verify.
+	 * @param id
+	 *            : id the type of novelty to verify.
+	 * @return NoveltyType: Object found with the search parameters.
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public NoveltyType nameExists(String name, int id) throws Exception {
+		StringBuilder query = new StringBuilder();
+		query.append("SELECT nt FROM NoveltyType nt ");
+		query.append("WHERE UPPER(nt.name)=UPPER(:name) ");
+		if (id != 0) {
+			query.append("AND nt.id <>:id ");
+		}
+		Query q = em.createQuery(query.toString());
+		q.setParameter("name", name);
+		if (id != 0) {
+			q.setParameter("id", id);
+		}
+		List<NoveltyType> results = q.getResultList();
+		if (results.size() > 0) {
+			return results.get(0);
+		}
+		return null;
+	}
 }
