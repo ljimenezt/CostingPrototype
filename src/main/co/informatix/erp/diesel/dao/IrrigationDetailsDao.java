e424a777b49fc55908d254b67ab47ff947cca4c0
ljimenez <ljimenez@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2017-03-01 14:05:55 +0000

    Add methods to consult

--- src/main/co/informatix/erp/diesel/dao/IrrigationDetailsDao.java
+++ src/main/co/informatix/erp/diesel/dao/IrrigationDetailsDao.java
@@ -1,10 +1,13 @@
 package co.informatix.erp.diesel.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.faces.model.SelectItem;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import co.informatix.erp.diesel.entities.IrrigationDetails;
 
@@ -56,4 +59,65 @@ public class IrrigationDetailsDao implements Serializable {
 		em.remove(em.merge(irrigationDetails));
 	}
 
+	/**
+	 * Returns the number of rows that exist in the database that are existing
+	 * or not existing.
+	 * 
+	 * @author Liseth.Jimenez
+	 * 
+	 * @param consult
+	 *            : Query running on SQL.
+	 * @param parameters
+	 *            :Parameters of the query.
+	 * @return Number of records found.
+	 * @throws Exception
+	 */
+	public Long quantityIrrigationDetails(StringBuilder consult,
+			List<SelectItem> parameters) throws Exception {
+		StringBuilder query = new StringBuilder();
+		query.append("SELECT COUNT(id) FROM IrrigationDetails id ");
+		query.append(consult);
+		Query q = em.createQuery(query.toString());
+		for (SelectItem parameter : parameters) {
+			q.setParameter(parameter.getLabel(), parameter.getValue());
+		}
+		return (Long) q.getSingleResult();
+	}
+
+	/**
+	 * Consult the list of irrigation details that comply with the option of
+	 * force.
+	 * 
+	 * @author Liseth.Jimenez
+	 * 
+	 * @param start
+	 *            : Registry where consultation begins.
+	 * @param range
+	 *            : Range of records.
+	 * @param consult
+	 *            : Consultation records depending on the parameters selected by
+	 *            the user.
+	 * @param parameters
+	 *            : Query parameters.
+	 * @return List<IrrigationDetails>:List of irrigation details that comply
+	 *         with the condition of validity.
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<IrrigationDetails> consultIrrigationDetails(int start,
+			int range, StringBuilder consult, List<SelectItem> parameters)
+			throws Exception {
+		StringBuilder query = new StringBuilder();
+		query.append("SELECT id FROM IrrigationDetails id ");
+		query.append("LEFT JOIN FETCH id.engineLog el ");
+		query.append("LEFT JOIN FETCH id.machine m ");
+		query.append("LEFT JOIN FETCH id.zone z ");
+		query.append(consult);
+		Query q = em.createQuery(query.toString());
+		for (SelectItem parameter : parameters) {
+			q.setParameter(parameter.getLabel(), parameter.getValue());
+		}
+		q.setFirstResult(start).setMaxResults(range);
+		return q.getResultList();
+	}
 }
