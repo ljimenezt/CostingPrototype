6e7f677ff43d10e6c62aae80e682eade2b56b334
lgranados <lgranados@bc4ab682-0880-ed4d-8b78-3b0d3b87262d>
2017-05-31 14:35:28 +0000

    Delete unused method consultAssistControl

--- src/main/co/informatix/erp/humanResources/dao/AssistControlDao.java
+++ src/main/co/informatix/erp/humanResources/dao/AssistControlDao.java
@@ -47,36 +47,6 @@ public class AssistControlDao implements Serializable {
 	}
 
 	/**
-	 * This method consult assist control list with a certain range sent as a
-	 * parameter and filtering the information by the values of sent search.
-	 * 
-	 * @param consult
-	 *            : Query records depending on the user selected parameter.
-	 * @param parameters
-	 *            : Query parameters.
-	 * @return List<AssistControl>: assist control list
-	 * @throws Exception
-	 */
-	@SuppressWarnings("unchecked")
-	public List<AssistControl> consultAssistControl(StringBuilder consult,
-			List<SelectItem> parameters) throws Exception {
-		StringBuilder query = new StringBuilder();
-		query.append("SELECT ac FROM AssistControl ac ");
-		query.append("JOIN FETCH ac.hr h ");
-		query.append(consult);
-		query.append("ORDER BY h.name ");
-		Query q = em.createQuery(query.toString());
-		for (SelectItem parameter : parameters) {
-			q.setParameter(parameter.getLabel(), parameter.getValue());
-		}
-		List<AssistControl> resultList = q.getResultList();
-		if (resultList.size() > 0) {
-			return resultList;
-		}
-		return null;
-	}
-
-	/**
 	 * This method allows consult if the worker is absent in the current day.
 	 * 
 	 * @param idHr
